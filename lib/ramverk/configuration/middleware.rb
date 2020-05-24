2f6f1882088d81e1b751486a8ff78ecdae7d397e
sandelius <tobias@sandeli.us>
2020-05-24 13:54:38 +0200

    feat: remove over ambitious middleware methods

--- lib/ramverk/configuration/middleware.rb
+++ lib/ramverk/configuration/middleware.rb
@@ -35,49 +35,6 @@ module Ramverk
         @stack.unshift [middleware, args, block].freeze
       end
 
-      # Prepend a middleware, before the specified, to the stack.
-      #
-      # @param lookup [Class] Lookup middleware.
-      # @param middleware [Class] Middleware class.
-      # @param *args [*] Middleware arguments.
-      # @param &block [Proc] Middleware block argument.
-      #
-      # @raise [RuntimeError] If the specified middleware is not found.
-      def before(lookup, middleware, *args, &block)
-        mw_index = index(lookup)
-
-        raise "#{lookup.name} could not be found in stack" unless mw_index
-
-        @stack.insert(mw_index, [middleware, args, block].freeze)
-      end
-
-      # Append a middleware, before the specified, to the stack.
-      #
-      # @param lookup [Class] Lookup middleware.
-      # @param middleware [Class] Middleware class.
-      # @param *args [*] Middleware arguments.
-      # @param &block [Proc] Middleware block argument.
-      #
-      # @raise [RuntimeError] If the specified middleware is not found.
-      def after(lookup, middleware, *args, &block)
-        mw_index = index(lookup)
-
-        raise "#{lookup.name} could not be found in stack" unless mw_index
-
-        @stack.insert(mw_index + 1, [middleware, args, block].freeze)
-      end
-
-      # Gets the stack index for the given middleware.
-      #
-      # If the middleware is not found in stack `nil` is returned.
-      #
-      # @param middleware [Class] Middleware to lookup.
-      #
-      # @return [Integer, nil]
-      def index(middleware)
-        @stack.index { |(mw, _, _)| mw == middleware }
-      end
-
       # @private
       def freeze
         @stack.freeze
--- spec/unit/configuration/middleware_spec.rb
+++ spec/unit/configuration/middleware_spec.rb
@@ -31,45 +31,5 @@ module Ramverk
           .to eq([Rack::MethodOverride, Rack::ETag])
       end
     end
-
-    describe "#before" do
-      it "prepends the middleware before the lookup" do
-        middleware.use Rack::ETag
-        middleware.use Rack::MethodOverride
-
-        middleware.before Rack::MethodOverride, Rack::ContentType
-
-        expect(middleware.stack.map(&:first))
-          .to eq([Rack::ETag, Rack::ContentType, Rack::MethodOverride])
-      end
-
-      it "raises an error if lookup is not found" do
-        middleware.use Rack::ETag
-
-        expect { middleware.before Rack::MethodOverride, Rack::ContentType }
-          .to raise_error(RuntimeError,
-                          "Rack::MethodOverride could not be found in stack")
-      end
-    end
-
-    describe "#after" do
-      it "prepends the middleware before the lookup" do
-        middleware.use Rack::ETag
-        middleware.use Rack::MethodOverride
-
-        middleware.after Rack::ETag, Rack::ContentType
-
-        expect(middleware.stack.map(&:first))
-          .to eq([Rack::ETag, Rack::ContentType, Rack::MethodOverride])
-      end
-
-      it "raises an error if lookup is not found" do
-        middleware.use Rack::ETag
-
-        expect { middleware.after Rack::MethodOverride, Rack::ContentType }
-          .to raise_error(RuntimeError,
-                          "Rack::MethodOverride could not be found in stack")
-      end
-    end
   end
 end
